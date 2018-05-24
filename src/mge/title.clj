2b878de2ac60605c9482afcb8b7176c03aa42574
Víctor M <victor@canelatools.com>
2018-05-24 12:19:54 +0200

    Minor fix

--- src/mge/title.clj
+++ src/mge/title.clj
@@ -2,9 +2,7 @@
   (:require [clj-z80.asm :refer :all :refer-macros :all]
             [clj-z80.msx.lib.bios :as bios]
             [clj-z80.msx.lib.sysvars :as sysvars]
-            [clj-z80.msx.lib.uncompress :refer [uncompress-lz77-to-vram]]
-            [clj-z80.msx.util.graphics :refer [convert-screen2]]
-            [clj-z80.msx.util.compress :refer [compress-lz77]]))
+            [clj-z80.msx.lib.uncompress :refer [uncompress-lz77-to-vram]]))
 
 (defasmproc load-patterns {:page :code}
   ;; HL=patterns
