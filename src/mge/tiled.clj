0c9b21b83adf3c8958503dbd57b6dc8423ddb605
Víctor M <samsaga2@gmail.com>
2019-04-21 11:46:03 +0200

    Refactoring

--- src/mge/tiled.clj
+++ src/mge/tiled.clj
@@ -52,33 +52,39 @@
           (range 256))))
 
 (defn- load-tileset
-  [m filename]
-  (if-let [source (get-attr (find-child-node m :tileset) :source)]
-    (load-tileset (xml/parse (get-filename-with-path filename source))
-                  filename)
+  [xml filename]
+  (if-let [source (get-attr (find-child-node xml :tileset) :source)]
+    ;; tileset xml is external
+    (let [xml-source (xml/parse (get-filename-with-path filename source))]
+      (load-tileset xml-source filename))
+    ;; tileset xml is already loaded
     (let [name         (.getName (io/file filename))
-          image-source (get-attr (find-child-node m :image) :source)
+          image-source (get-attr (find-child-node xml :image) :source)
           image-source (get-filename-with-path filename image-source)]
       {:tilemap   image-source
-       :tiletypes (get-tileset-types m)})))
+       :tiletypes (get-tileset-types xml)})))
+
+(defn- parse-cells
+  [layer]
+  (let [data  (find-child-node layer :data)
+        cells (str/join "" (map #(str/replace % #"(\n|\r|\s)+" "")
+                                (:content data)))]
+    (->> (str/split cells #",")
+         (map try-parse-int)
+         (mapv dec))))
 
 (defn load-tilemap
   [filename]
-  (let [m         (xml/parse filename)
-        direction (get-attr (find-property-node m "direction") :value)
-        layer     (find-child-node m :layer)
+  (let [xml       (xml/parse filename)
+        direction (get-attr (find-property-node xml "direction") :value)
+        layer     (find-child-node xml :layer)
         w         (try-parse-int (get-attr layer :width))
         h         (try-parse-int (get-attr layer :height))
-        data      (find-child-node layer :data)
-        cells     (str/join "" (map #(str/replace % #"(\n|\r|\s)+" "")
-                                    (:content data)))
-        cells     (->> (str/split cells #",")
-                       (map try-parse-int)
-                       (mapv dec))]
-    (merge {:tile-width  (try-parse-int (get-attr m :tilewidth))
-            :tile-height (try-parse-int (get-attr m :tileheight))
+        cells     (parse-cells layer)]
+    (merge {:tile-width  (try-parse-int (get-attr xml :tilewidth))
+            :tile-height (try-parse-int (get-attr xml :tileheight))
             :width       w
             :height      h
             :direction   direction
             :cells       cells}
-           (load-tileset m filename))))
+           (load-tileset xml filename))))
